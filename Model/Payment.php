7551a864bf44d1ccf4dddab718df51f75d8a6ab8
Jose Guadalupe Romero <jose.romero@openpay.mx>
2020-08-14 08:13:33 -0500

    Unificación de librerías MEX/CO para versiones de Magento >= 2.3.0

--- Model/Payment.php
+++ Model/Payment.php
@@ -527,7 +527,7 @@ class Payment extends \Magento\Payment\Model\Method\AbstractMethod
             )
         );
 
-        $openpay = \Openpay::getInstance($this->merchant_id, $this->sk);
+        $openpay = \Openpay::getInstance($this->merchant_id, $this->sk, $this->country);
         \Openpay::setSandboxMode($this->is_sandbox);
 
         $userAgent = "Openpay-MTO2".$this->country."/v2";
@@ -574,7 +574,7 @@ class Payment extends \Magento\Payment\Model\Method\AbstractMethod
     }
 
     public function getOpenpayInstance() {
-        $openpay = \Openpay::getInstance($this->merchant_id, $this->sk);
+        $openpay = \Openpay::getInstance($this->merchant_id, $this->sk, $this->country);
         \Openpay::setSandboxMode($this->is_sandbox);
         
         $userAgent = "Openpay-MTO2".$this->country."/v2";
