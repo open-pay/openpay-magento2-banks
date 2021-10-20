7a6bb03706d6da38f3114980c4283b23f0f2a548
Jose Guadalupe Romero <jose.romero@openpay.mx>
2021-10-19 21:38:33 -0500

    Actualización de la librería openpay/sdk v2.2.*

--- Model/Payment.php
+++ Model/Payment.php
@@ -13,6 +13,8 @@ use Magento\Store\Model\ScopeInterface;
 use Magento\Customer\Model\Customer;
 use Magento\Customer\Model\Session as CustomerSession;
 
+use Openpay\Data\Client as Openpay;
+
 /**
  * Class Payment
  *
@@ -553,11 +555,11 @@ class Payment extends \Magento\Payment\Model\Method\AbstractMethod
     }
 
     public function getOpenpayInstance() {
-        $openpay = \Openpay::getInstance($this->merchant_id, $this->sk, $this->country);
-        \Openpay::setSandboxMode($this->is_sandbox);
+        $openpay = Openpay::getInstance($this->merchant_id, $this->sk, $this->country);
+        Openpay::setSandboxMode($this->is_sandbox);
         
         $userAgent = "Openpay-MTO2".$this->country."/v2";
-        \Openpay::setUserAgent($userAgent);
+        Openpay::setUserAgent($userAgent);
         
         return $openpay;
     }
